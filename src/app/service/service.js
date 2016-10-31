c081a6269ff976f36a8ddaf1fa9e47fe4b2fbf5a
Nicolay David Mohebi <nicolaydmohebi@gmail.com>
2016-10-31 20:01:03 +0100

    Added service.js

---
+++ src/app/service/service.js
@@ -0,0 +1,56 @@
+import url from '../conf/url';
+import axios from 'axios'
+
+export function getNamespaces(){
+	axios.get(`${url}/api/25/dataStore/`)
+    .then((response) =>  {
+			return response.data;
+		})
+		.catch((error) => {
+			return error;
+		});
+}
+
+export function getKeys(namespace){
+  axios.get(`${url}/api/25/dataStore/${namespace}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      return error;
+    });
+}
+
+export function putKey(namespace, key, encrypted){
+  let encrypt = '';
+  if(encrypted) encrypt = '?encrypt=true'
+
+	axios.post(`${url}/api/25/dataStore/${namespace}/${key + encrypt}`)
+		.then((response) =>  {
+			return response.data;
+		})
+		.catch((error) =>{
+			return error;
+		});
+}
+
+
+export function deleteKeys(namespace){
+  axios.delete(`${url}/api/25/dataStore/${namespace}`$)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      return error;
+    });
+}
+
+export function deleteKey(namespace, key){
+  axios.delete(`${url}/api/25/dataStore/${namespace}/${key}`$)
+    .then((response) =>  {
+      return response.data;
+    })
+    .catch((error) =>{
+      return error;
+    });
+}
