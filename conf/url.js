b52b5aa1793776103d9ed469ac76591d393d046b
Nicolay David Mohebi <nicolaydmohebi@gmail.com>
2016-10-31 21:33:46 +0100

    Added axios

--- conf/url.js
+++
@@ -1,1 +0,0 @@
-export default cons process.env.BASE_URL || "play.dhis2.org/demo/";
--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import {Provider} from 'react-redux';
 import App from './app/containers/App';
 import configureStore from './app/store/configureStore';
 import {Router, Route, browserHistory} from 'react-router';
-
+import {getNamespaces/* , getKeys, putKey, deleteKey, deleteKeys */} from "./app/service/service";
 import 'todomvc-app-css/index.css';
 
 const store = configureStore();
-
+console.log(getNamespaces());
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
