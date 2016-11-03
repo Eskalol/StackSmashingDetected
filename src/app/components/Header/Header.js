11413fa37e6824f22aa6462bfb37e9f49b224b4c
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-11-03 16:43:20 +0100

    fixed test for header

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 // import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
-class Header extends Component {
+export class Header extends Component {
 
   constructor(props) {
     super(props);
