5a117068417d558cc4088bc65d47ccf2a64f6831
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-11-03 06:41:10 +0100

    fixed some styling, containers etc...

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -8,8 +8,11 @@ export default class extends Component {
         <div className="header-content">
           <div className="row">
             <div className="col-sm-3"></div>
-            <div className="col-sm-6"><h1>Cool</h1></div>
-            <div className="col-sm-3"></div>
+            <div className="col-sm-5"><h1>DHIS</h1></div>
+            <div className="col-sm-4">
+              <i className="fa fa-search fa-2x" aria-hidden="true"/>
+              <input type="text" id="search" className="input-box" placeholder="Search..."/>
+            </div>
           </div>
         </div>
       </header>
