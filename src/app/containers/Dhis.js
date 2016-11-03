4d27b0b598e98b338b382bae6c2515bfa2549648
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-11-03 14:09:53 +0100

    added textarea styling!

--- src/app/containers/Dhis.js
+++ src/app/containers/Dhis.js
@@ -60,6 +60,12 @@ class Dhis extends Component {
           <div className="col-md-2 container">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
           </div>
+          <div className="col-md-2">
+            <div className="container-header">Text area example</div>
+            <div className="container">
+              <textarea className="text-area" rows="10" cols="30"/>
+            </div>
+          </div>
         </div>
       </div>
     );
