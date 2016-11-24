299eab7437c23298f7ab874adb8ed87fbb20bf77
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-11-24 11:00:06 +0100

    fixed tests for namespace container

---
+++ src/app/containers/Namespace.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import {Namespace} from './Namespace';
+import {Provider} from 'react-redux';
+import configureStore from '../store/configureStore';
+
+const store = configureStore();
+
+const props = {
+  namespaceName: "SuperCoolNamespace",
+  actions: {
+    changeText: jasmine.createSpy(),
+    analysisButton: jasmine.createSpy(),
+    analysisListUrl: jasmine.createSpy()
+  }
+};
+
+describe('Containers', () => {
+  describe('<Namespace/>', () => {
+    it('should fire actions correctly', () => {
+      const wrapper = mount(
+        <Provider store={store}>
+          <Namespace {...props}/>
+        </Provider>
+      );
+      expect(wrapper.find(Namespace).props().actions.changeText).toHaveBeenCalledWith("SuperCoolNamespace");
+      expect(wrapper.find(Namespace).props().actions.analysisButton).toHaveBeenCalledWith(true);
+      expect(wrapper.find(Namespace).props().actions.analysisListUrl).toHaveBeenCalledWith(`/namespace-analysis?name=${props.namespaceName}`, false);
+    });
+  });
+});
