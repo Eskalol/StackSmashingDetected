5d692362bbf73d2c283aadf7749450e0f68e80d7
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-11-24 09:04:31 +0100

    fixed footer tests with enzyme

--- src/app/components/Header/Header.spec.js
+++ src/app/components/Header/Header.spec.js
@@ -11,29 +11,28 @@ const props = {
 
 describe('Components', () => {
   describe('<Header/>', () => {
-    it('should render header render correctly', () => {
+    it('should render header correctly', () => {
       const wrapper = shallow(<Header {...props}/>);
       expect(wrapper.is('.header')).toBe(true);
 
-      expect(wrapper.find('.header-content').length).toBe(1);
-      expect(wrapper.text()).toBe(props.ht);
-      expect(wrapper.find('.row').length).toBe(1);
-      expect(wrapper.find('.col-sm-3').length).toBe(1);
-      expect(wrapper.find('.col-sm-4').length).toBe(1);
-      expect(wrapper.find('.col-sm-5 #header-text').length).toBe(1);
-      expect(wrapper.find('div > h1').length).toBe(1);
+      expect(wrapper.find('div .header-content').length).toBe(1);
+      expect(wrapper.find('div .col-sm-5 > h1').text()).toBe(props.ht);
+      expect(wrapper.find('div .row').length).toBe(1);
+      expect(wrapper.find('div .col-sm-3').length).toBe(1);
+      expect(wrapper.find('div .col-sm-4').length).toBe(1);
+      expect(wrapper.find('div .col-sm-5 #header-text > h1').length).toBe(1);
     });
     it('should render serach correctly', () => {
       const wrapper = shallow(<Header {...props}/>);
       expect(wrapper.find('input #search').length).toBe(1);
-      expect(wrapper.find('.fa .fa-search .fa-2x').length).toBe(1);
+      expect(wrapper.find('i .fa .fa-search .fa-2x').length).toBe(1);
     });
     it('should render analysis button correctly', () => {
       const wrapper = shallow(<Header {...props}/>);
-      expect(wrapper.find('.analysis-button').length).toBe(1);
+      expect(wrapper.find('div .analysis-button').length).toBe(1);
       expect(wrapper.find('a[href="/someUrl-analysis"]').length).toBe(1);
-      expect(wrapper.find('.fa .fa-bar-chart .fa-2x .fa-background').length).toBe(1);
-      expect(wrapper.find('.fa .fa-table .fa-2x .fa-background').length).toBe(0);
+      expect(wrapper.find('i .fa .fa-bar-chart .fa-2x .fa-background').length).toBe(1);
+      expect(wrapper.find('i .fa .fa-table .fa-2x .fa-background').length).toBe(0);
     });
     it('should render table button correctly', () => {
       const _props = Object.assign({}, props, {
@@ -41,20 +40,20 @@ describe('Components', () => {
         analysisUrl: "/someUrl"
       });
       const wrapper = shallow(<Header {..._props}/>);
-      expect(wrapper.find('.analysis-button').length).toBe(1);
+      expect(wrapper.find('div .analysis-button').length).toBe(1);
       expect(wrapper.find('a[href="/someUrl"]').length).toBe(1);
-      expect(wrapper.find('.fa .fa-bar-chart .fa-2x .fa-background').length).toBe(0);
-      expect(wrapper.find('.fa .fa-table .fa-2x .fa-background').length).toBe(1);
+      expect(wrapper.find('i .fa .fa-bar-chart .fa-2x .fa-background').length).toBe(0);
+      expect(wrapper.find('i .fa .fa-table .fa-2x .fa-background').length).toBe(1);
     });
     it('should not render analysis or table button', () => {
       const _props = Object.assign({}, props, {
         analysisButton: false
       });
       const wrapper = shallow(<Header {..._props}/>);
-      expect(wrapper.find('.analysis-button').length).toBe(1);
+      expect(wrapper.find('div .analysis-button').length).toBe(1);
       expect(wrapper.find('a[href="/someUrl-analysis"]').length).toBe(0);
-      expect(wrapper.find('.fa .fa-bar-chart .fa-2x .fa-background').length).toBe(0);
-      expect(wrapper.find('.fa .fa-table .fa-2x .fa-background').length).toBe(0);
+      expect(wrapper.find('i .fa .fa-bar-chart .fa-2x .fa-background').length).toBe(0);
+      expect(wrapper.find('i .fa .fa-table .fa-2x .fa-background').length).toBe(0);
     });
   });
 });
