7f05766d13e48ee4296f4924f34f30c6cdd19e67
torstbre <torstbre@student.matnat.uio.no>
2016-11-28 00:00:19 +0100

    Removed old code and cleand up NamespaceAnalysis

--- src/app/containers/NamespaceAnalysis.js
+++ src/app/containers/NamespaceAnalysis.js
@@ -22,29 +22,21 @@ export class NamespaceAnalysis extends Component {
     actions.analysisListUrl(`/namespace?name=${this.props.namespaceName}`, true);
 
     this.props.actions.getKeys(this.props.namespaceName);
-    // console.log("this.props.actions: ", this.props.actions);
   }
 
-  // Dates, how many keys have been edited on one day
-
+  // Return the specified graph
   getGraph(data) {
     return <Radar data={data} options={chartOptions}/>;
   }
 
+  // Remove the timestamp, everything after 'T'
+  // 'date' are strings like this: "2016-11-27T20:23:38.144"
   removeTimeStamp(date) {
-    // return dates.map(elem => (
     return date.substring(0, date.indexOf('T'));
-    // ));
   }
 
+  // Get all the unique days from the input data
   getUniqueDays(inputData) {
-    /* const uniqueDays = [];
-    dates.forEach(elem => {
-      if (!uniqueDays.includes(elem)) {
-        uniqueDays.push(elem);
-      }
-    });
-    return uniqueDays;*/
     const uniqueDays = [];
     inputData.forEach(elem => {
       if (!uniqueDays.includes(elem.created)) {
@@ -57,76 +49,27 @@ export class NamespaceAnalysis extends Component {
     return uniqueDays;
   }
 
-  // dates are each of the dates a key was created/edited
-  // uniqueDates are the uniqueDates of dates
-  // We want to know how many of the uniqueDates
-  getKeysPerDay(dates, uniqueDates) {
-    const keysPerDay = uniqueDates.map(() => 0);
-    console.log(keysPerDay);
-    uniqueDates.forEach((day, i) => {
-      dates.forEach(date => {
-        if (day === date) {
-          keysPerDay[i] += 1;
-        }
-      });
-    });
-    return keysPerDay;
-  }
-
-  // Create dataset for line, bar and radar chart
+  // Create dataset for line, bar or radar chart
   createDataSetType1(inputData) {
-    // Get all the dates from inputData
-    // let creationDates = [];
-    // let lastEditDates = [];
-
-    console.log(inputData);
-
     // We are not interested in the timestamp, only the date
     inputData.forEach(elem => {
       elem.created = this.removeTimeStamp(elem.created);
       elem.lastUpdated = this.removeTimeStamp(elem.lastUpdated);
     });
-    console.log(inputData);
 
+    // Getting all the unique dates in a way that if there are dates a key was
+    // created but none updated (and vice versa) the date is still included
     const uniqueDates = this.getUniqueDays(inputData);
-    console.log(uniqueDates);
 
     // Creating arrays with as many zeroes as uniqueDates is long
     const creationDataSet = uniqueDates.map(() => (0));
     const lastEditDataSet = uniqueDates.map(() => (0));
 
+    // Count how many keys were created and updated on each date
     inputData.forEach(elem => {
       creationDataSet[uniqueDates.indexOf(elem.created)]++;
       lastEditDataSet[uniqueDates.indexOf(elem.lastUpdated)]++;
     });
-
-    /* creationDates = this.removeTimeStamp(creationDates);
-    lastEditDates = this.removeTimeStamp(lastEditDates);
-
-    const dates = [];
-    creationDates.forEach(elem => {
-      if (!dates.include(elem)) {
-        dates.push(elem);
-      }
-    });
-    lastEditDates.forEach(elem => {
-      if (!dates.include(elem)) {
-        dates.push(elem);
-      }
-    });
-
-    console.log(dates.length, creationDates.length, lastEditDates.length);
-
-    const creationUniqueDates = this.getUniqueDays(creationDates);
-    // Now we have all the unique days where keys were created
-    // Need to find out how many keys where created for each day
-    const creationKeysPerDay = this.getKeysPerDay(creationDates, creationUniqueDates);
-
-    const lastEditUniqueDates = this.getUniqueDays(lastEditDates);
-    const lastEditKeysPerDay = this.getKeysPerDay(lastEditDates, lastEditUniqueDates);
-
-    // Merge last edit days with the creation dates for one set of labels
-    // Adding all the days from last edit to creation*/
 
     return {
       labels: uniqueDates,
