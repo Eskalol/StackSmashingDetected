f4dda2c0332b1bdcc4ca8e35136e7c6050facc74
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-11-30 01:22:18 +0100

    added namespace action tests

---
+++ src/app/actions/namespace.spec.js
@@ -0,0 +1,17 @@
+import {REQUEST_NAMESPACES, RECEIVE_NAMESPACES} from '../constants/namespaceTypes';
+import * as actions from './namespace';
+
+describe('Namespace actions', () => {
+  it('should create REQUEST_NAMESPACES action', () => {
+    expect(actions.requestNamespaces()).toEqual({
+      type: REQUEST_NAMESPACES
+    });
+  });
+  it('should create RECEIVE_NAMESPACES action', () => {
+    expect(actions.receiveNamespaces('some lol')).toEqual({
+      type: RECEIVE_NAMESPACES,
+      namespaces: 'some lol',
+      receivedAt: Date.now()
+    });
+  });
+});
