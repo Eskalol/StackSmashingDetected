4141948e538807fad20a0a664c532872ef6cd150
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-11-30 01:31:23 +0100

    added tests for analysis actions

---
+++ src/app/actions/analysis.spec.js
@@ -0,0 +1,31 @@
+import {REQUEST_KEYS, RECEIVE_KEYS, REQUEST_NAMESPACES, RECEIVE_NAMESPACES} from '../constants/AnalysisTypes';
+import * as actions from './analysis';
+
+describe('Analysis actions', () => {
+  it('should create REQUEST_KEYS action', () => {
+    expect(actions.requestKeys()).toEqual({
+      type: REQUEST_KEYS
+    });
+  });
+  it('should create RECEIVE_KEYS action', () => {
+    expect(actions.receiveKeys(['cool', 'lol'], 'cool')).toEqual({
+      type: RECEIVE_KEYS,
+      receivedAt: Date.now(),
+      keyCnt: 2,
+      namespace: 'cool'
+    });
+  });
+  it('should create RECEIVE_NAMESPACES', () => {
+    expect(actions.receiveNamespaces(['imba', 'lol', 'cool'])).toEqual({
+      type: RECEIVE_NAMESPACES,
+      itemCnt: 3,
+      namespaces: ['imba', 'lol', 'cool'],
+      receivedAt: Date.now()
+    });
+  });
+  it('should create REQUEST_NAMESPACES action', () => {
+    expect(actions.requestNamespaces()).toEqual({
+      type: REQUEST_NAMESPACES
+    });
+  });
+});
